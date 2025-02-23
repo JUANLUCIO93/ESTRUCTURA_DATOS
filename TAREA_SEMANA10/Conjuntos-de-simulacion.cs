fbee40ae161811e4d555f962f6247d39e0c4e7c7
JUANLUCIO93 <159538340+JUANLUCIO93@users.noreply.github.com>
2025-02-23 17:37:23 -0500

    Tarea, semana 10

---
+++ TAREA_SEMANA10/Conjuntos-de-simulacion.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // Generación de datos ficticios
+        HashSet<string> ciudadanos = GenerarCiudadanos(500);
+        HashSet<string> vacunadosPfizer = GenerarCiudadanos(75);
+        HashSet<string> vacunadosAstrazeneca = GenerarCiudadanos(75);
+        HashSet<string> vacunadosAmbos = GenerarCiudadanos(100);
+        
+        // Ciudadanos vacunados con ambas dosis (asumimos que ambos conjuntos contienen ciudadanos únicos)
+        HashSet<string> vacunadosDosDosis = new HashSet<string>(vacunadosAmbos);
+        
+        // Ciudadanos vacunados con solo Pfizer
+        HashSet<string> soloPfizer = new HashSet<string>(vacunadosPfizer);
+        soloPfizer.ExceptWith(vacunadosAstrazeneca);
+        soloPfizer.ExceptWith(vacunadosDosDosis);
+        
+        // Ciudadanos vacunados con solo Astrazeneca
+        HashSet<string> soloAstrazeneca = new HashSet<string>(vacunadosAstrazeneca);
+        soloAstrazeneca.ExceptWith(vacunadosPfizer);
+        soloAstrazeneca.ExceptWith(vacunadosDosDosis);
+        
+        // Ciudadanos no vacunados
+        HashSet<string> noVacunados = new HashSet<string>(ciudadanos);
+        noVacunados.ExceptWith(vacunadosPfizer);
+        noVacunados.ExceptWith(vacunadosAstrazeneca);
+        noVacunados.ExceptWith(vacunadosDosDosis);
+        
+        // Impresión de resultados
+        Console.WriteLine("Listado de ciudadanos no vacunados: " + noVacunados.Count);
+        Console.WriteLine("Listado de ciudadanos con dos dosis: " + vacunadosDosDosis.Count);
+        Console.WriteLine("Listado de ciudadanos vacunados solo con Pfizer: " + soloPfizer.Count);
+        Console.WriteLine("Listado de ciudadanos vacunados solo con Astrazeneca: " + soloAstrazeneca.Count);
+    }
+
+    static HashSet<string> GenerarCiudadanos(int cantidad)
+    {
+        HashSet<string> ciudadanos = new HashSet<string>();
+        Random rnd = new Random();
+        while (ciudadanos.Count < cantidad)
+        {
+            ciudadanos.Add("Ciudadano_" + rnd.Next(1, 10000));
+        }
+        return ciudadanos;
+    }
+}
